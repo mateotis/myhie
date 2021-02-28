659da115bcd661c47e2c05906e8ea2fde5355b12
mateotis <hekfuszmate@gmail.com>
2021-02-28 20:00:15 +0400

    Added actual data and exec test files. Also in the process of rewriting parsing code.

--- myhie.cpp
+++ myhie.cpp
@@ -2,8 +2,24 @@
 #include <fstream>
 #include <string> // Yay!
 #include <cstring> // For strcmp
+#include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/wait.h>
 
 using namespace std;
+
+static void showReturnStatus(pid_t childpid, int status)
+{
+	if (WIFEXITED(status) && !WEXITSTATUS(status))
+		printf("Child %ld terminated normally\n", (long)childpid);
+	else if (WIFEXITED(status))
+		printf("Child %ld terminated with return status %d\n", (long)childpid, WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		printf("Child %ld terminated due to uncaught signal %d\n", (long)childpid, WTERMSIG(status));
+	else if (WIFSTOPPED(status))
+		printf("Child %ld stopped due to signal %d\n", (long)childpid, WSTOPSIG(status));
+}
 
 int main(int argc, char* args[]) {
 
@@ -50,6 +66,8 @@ int main(int argc, char* args[]) {
 		return -1;
 	}
 
+	//execl("./execTest", "execTest", NULL);
+
 	// Construct and print a neat little program configuration based on the command line parametres
 	cout << "PROGRAM SETTINGS" << endl << "Input file: " << inputFile << endl << "Output file: " << outputFile << endl << "Number of sorters: " << workerNum << endl << "Attribute to sort by: ";
 	if(attrNum == 0) {
@@ -80,7 +98,6 @@ int main(int argc, char* args[]) {
 	}
 	cout << "\n" << endl;
 
-	string line;
 	ifstream fin;
 	fin.open(inputFile);
 	if(!fin) { // Fallback in case we can't open the file for whatever reason
@@ -93,11 +110,12 @@ int main(int argc, char* args[]) {
 		string firstName;
 		string lastName;
 		int dep;
-		int income;
+		float income;
 		int zip;		
 	};
 
-	int rid, dep, income, zip;
+	int rid, dep, zip;
+	float income;
 	string firstName, lastName;
 
 	int lineCount = 0;
@@ -109,9 +127,27 @@ int main(int argc, char* args[]) {
 	fin.open(inputFile); // Opening it again to reset the file reader to the top
 
 	int i = 0;
+	lineCount = 1000;
 	Taxpayer dataSet[lineCount]; // Array of Taxpayer structs, this is what we'll slice up and pass to all our sorters
 
-	while (fin >> rid >> firstName >> lastName >> dep >> income >> zip) { // As with the previous assignment, this syntax makes parsing easy - even easier, in fact, as we have strings instead of char arrays and don't have to bother with memory allocation
+	string line;
+	int readVar; // Which variable we're currently trying to assemble
+	while(getline(fin, line)) { // The random number of spaces between variables is very problematic in trying to parse file input, so we have to construct our strings carefully
+		cout << line << endl;
+		string rid = "";
+		for(int i = 0; i < line.length(); i++) {
+			if(line[i] != ' ') {
+				rid += line[i];
+			}
+			else {
+				cout << "Ran into space!" << endl;
+				break;
+			}
+		}
+		cout << rid << endl;
+	}
+
+/*	while (fin >> rid >> firstName >> lastName >> dep >> income >> zip) { // As with the previous assignment, this syntax makes parsing easy - even easier, in fact, as we have strings instead of char arrays and don't have to bother with memory allocation
 		if(i > lineCount-1) { // Makes sure we don't go out of bounds
 			break;
 		}
@@ -123,11 +159,11 @@ int main(int argc, char* args[]) {
 		dataSet[i].zip = zip;
 		i++;
 
-	}
+	}*/
 	fin.close();
 
 	for(unsigned int i = 0; i < sizeof(dataSet)/sizeof(dataSet[0]); i++) { // Print results of parsing
-		cout << dataSet[i].rid << " " << dataSet[i].firstName << " " << dataSet[i].lastName << " " << dataSet[i].dep << " " << dataSet[i].income << " " << dataSet[i].zip << endl;
+		//cout << dataSet[i].rid << " " << dataSet[i].firstName << " " << dataSet[i].lastName << " " << dataSet[i].dep << " " << dataSet[i].income << " " << dataSet[i].zip << endl;
 	}
 
 	return 0;
